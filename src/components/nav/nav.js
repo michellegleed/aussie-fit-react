95fa323c7c9911730f7ad3a6a2588127e4dca740
Michelle Gleed <gleed.michelle@gmail.com>
2021-02-08 15:57:33 +0800

    Nav.js updates itself based on user permissions

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,22 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import { Link, NavLink, useLocation } from "react-router-dom";
+import { fetchRequest } from '../../utils/fetchRequest';
 
 function Nav() {
 
-    /// Update Nav Based on Logged In/Out
-    const [loggedIn, setLoggedIn] = useState(false);
-
     // location variable will update whenever the react app's url changes
     const location = useLocation();
+
+    /// Update Nav Based on Logged In/Out and logged in user isAdmin or not
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [isAdmin, setIsAdmin] = useState();
+
+    const checkUserPermissions = () => {
+        fetchRequest(`${process.env.REACT_APP_API_URL}user/`)
+            .then((result) => {
+                if (result.ok) {
+                    setIsAdmin(result.data.is_admin);
+                }
+                else {
+                    console.log("couldn't het user permissions");
+                }
+            })
+    }
+
 
     // runs @ first render and whenever url location changes
     useEffect(() => {
         const token = window.localStorage.getItem("token");
         if (token) {
             setLoggedIn(true);
+            if (isAdmin == null) {
+                checkUserPermissions();
+            }
         }
         else {
             setLoggedIn(false);
+            setIsAdmin();
         }
     }, [location]);
 
@@ -34,19 +53,20 @@ function Nav() {
                 </div>
                 {loggedIn ?
                     <div className="nav-menu-items">
-                        {/* check if user is admin */}
-                        <React.Fragment>
+                        {isAdmin ?
+                            <React.Fragment>
+                                <div className="nav-item">
+                                    <NavLink to="/admin" activeStyle={{ color: 'rgb(4, 180, 4)' }}>Admin</NavLink>
+                                </div>
+                                <div className="nav-item">
+                                    <a href={`${process.env.REACT_APP_API_URL}participants/attendance-to-csv/`}>Download Attendance Data</a>
+                                </div>
+                            </React.Fragment>
+                            :
                             <div className="nav-item">
-                                <NavLink to="/admin" activeStyle={{ color: 'rgb(4, 180, 4)' }}>Admin</NavLink>
+                                <NavLink exact to="/" activeStyle={{ color: 'rgb(4, 180, 4)' }}>Home</NavLink>
                             </div>
-                            <div className="nav-item">
-                                <a href={`${process.env.REACT_APP_API_URL}participants/attendance-to-csv/`}>Download Attendance Data</a>
-                            </div>
-                        </React.Fragment>
-                        {/* : */}
-                        <div className="nav-item">
-                            <NavLink exact to="/" activeStyle={{ color: 'rgb(4, 180, 4)' }}>Home</NavLink>
-                        </div>
+                        }
                         <Link to="/login" onClick={handleLogout}>Log Out</Link>
                     </div>
                     :
