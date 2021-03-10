8718e35d0bb9ab1c050e8118e7a55b1b2b1b1f92
Michelle Gleed <gleed.michelle@gmail.com>
2021-03-11 07:24:11 +0800

    Changed searchForm styling based on media query (and nav theme)

--- src/components/dashboard/searchForm/searchForm.js
+++ src/components/dashboard/searchForm/searchForm.js
@@ -35,53 +35,102 @@ function SearchForm() {
             })
     }, []);
 
-    const useStyles = makeStyles({
-        // root: {
-        //     color: "white",
-        //     backgroundColor: "transparent",
-        //     "&.Mui-focused": {
-        //         color: "white",
-        //         backgroundColor: "transparent"
-        //     },
-        root: {
-            color: "white",
-            "&.Mui-focused": {
-                color: "white",
-            },
-            "&:before": {
-                borderBottomColor: "white"
-            },
-            "&:hover:not(.Mui-focused):before": {
-                borderBottomColor: "white"
-            },
-            "&:after": {
-                // focused
-                borderBottomColor: "white"
-            }
-        },
-        input: {
-            color: "white",
-            "&.Mui-focused": {
-                color: "white",
-            },
-            "&::selection": {
-                backgroundColor: "transparent",
-                color: "white"
-            }
-        }
+    const mediaQueryMatch = window.matchMedia('(min-width: 720px)');
+    const [matches, setMatches] = useState(mediaQueryMatch.matches)
+
+    useEffect(() => {
+        const changeHandler = e => setMatches(e.matches);
+        mediaQueryMatch.addListener(changeHandler);
+        return () => mediaQueryMatch.removeListener(changeHandler);
     });
 
-    const useLabelStyles = makeStyles({
-        root: {
-            color: "white",
-            "&.Mui-focused": {
-                color: "white"
+    const getStyles = (isMinMediaSize) => {
+        const mainColor = isMinMediaSize ? "black" : "white";
+        console.log("main color is ", mainColor);
+        return makeStyles({
+            root: {
+                color: mainColor,
+                "&.Mui-focused": {
+                    color: mainColor,
+                },
+                "&:before": {
+                    borderBottomColor: mainColor
+                },
+                "&:hover:not(.Mui-focused):before": {
+                    borderBottomColor: mainColor
+                },
+                "&:after": {
+                    // focused
+                    borderBottomColor: mainColor
+                }
+            },
+            input: {
+                color: mainColor,
+                "&.Mui-focused": {
+                    color: mainColor,
+                },
+                "&::selection": {
+                    backgroundColor: "transparent",
+                    color: mainColor
+                }
             }
-        }
-    });
+        })
+    }
 
-    const classes = useStyles();
-    const labelClasses = useLabelStyles();
+    // const useStyles = makeStyles({
+    //     // root: {
+    //     //     color: "white",
+    //     //     backgroundColor: "transparent",
+    //     //     "&.Mui-focused": {
+    //     //         color: "white",
+    //     //         backgroundColor: "transparent"
+    //     //     },
+    //     root: {
+    //         color: "white",
+    //         "&.Mui-focused": {
+    //             color: "white",
+    //         },
+    //         "&:before": {
+    //             borderBottomColor: "white"
+    //         },
+    //         "&:hover:not(.Mui-focused):before": {
+    //             borderBottomColor: "white"
+    //         },
+    //         "&:after": {
+    //             // focused
+    //             borderBottomColor: "white"
+    //         }
+    //     },
+    //     input: {
+    //         color: "white",
+    //         "&.Mui-focused": {
+    //             color: "white",
+    //         },
+    //         "&::selection": {
+    //             backgroundColor: "transparent",
+    //             color: "white"
+    //         }
+    //     }
+    // });
+
+    const getLabelStyles = (isMinMediaSize) => {
+        const mainColor = isMinMediaSize ? "black" : "white";
+        // const useLabelStyles = makeStyles({
+        return makeStyles({
+            root: {
+                color: mainColor,
+                "&.Mui-focused": {
+                    color: mainColor
+                }
+            }
+        })
+    }
+
+    // const classes = useStyles();
+    // const labelClasses = useLabelStyles();
+
+    const classes = getStyles(matches);
+    const labelClasses = getLabelStyles(matches);
 
     return (
         participantList != null ?
@@ -90,12 +139,13 @@ function SearchForm() {
                     <Autocomplete
                         id="search-box"
                         classes={classes}
+                        // classes={getStyles(matches)}
                         options={participantList}
                         getOptionLabel={(option) => option.first_name + " " + option.last_name}
-                        style={{ width: 220, color: "white" }}
+                        style={{ width: 220, color: matches ? "black" : "white" }}
                         size="small"
-                        renderInput={(params) => <TextField {...params} variant="outlined" autoComplete="off" label="Search Participants" InputProps={{ ...params.InputProps, classes: classes, style: { color: "#fff" } }}
-                            InputLabelProps={{ ...params.InputLabelProps, classes: labelClasses }}
+                        renderInput={(params) => <TextField {...params} variant="outlined" autoComplete="off" label="Search Participants" InputProps={{ ...params.InputProps, classes: classes, style: { color: matches ? "black" : "white" } }}
+                            InputLabelProps={{ ...params.InputLabelProps, classes: labelClasses, style: { color: matches ? "black" : "white" } }}
                         />}
                         onChange={(e, value) => setParticipantID(value != null ? value.id : null)}
                     />
